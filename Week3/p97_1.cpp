b05284f1463fc3c2b32c1e9baa931403aaf5c85d
hwanwoong0903 <ghksdnd02@gmail.com>
2026-03-17 16:45:29 +0900

    seejo

--- Week3/p97_1.cpp
+++ Week3/p97_1.cpp
@@ -1,3 +1,4 @@
+//  1 번
 #include<iostream>
 
 int main(){
@@ -12,5 +13,67 @@ int main(){
         std::cout << "\n";
     }
 
+    return 0;
+}
+
+// 2번
+#include<iostream>
+
+int main()
+{
+    for (int i = 1; i <= 5 ; i++)
+    {
+        for (int j = 5; j >= i; j--)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
+//  3번
+
+#include<iostream>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int k = 4; k >= i; k--)
+        {
+            std::cout << " ";
+        }
+        for (int j = 0; j <= i - 1; j++)
+        {
+            std::cout << i - j;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
+// 4번
+#include<iostream>
+
+int main(){
+
+    for (int i = 1; i <= 5; i ++)
+    {
+        for (int k = 4; k >= i; k--)
+        {
+            std::cout << " ";
+        }
+         for (int s = 0; s < i; s++)
+        {
+            std::cout << i - s;
+        }
+        for(int j = 2; j <= i; j++)
+        {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }
+    
     return 0;
 }
